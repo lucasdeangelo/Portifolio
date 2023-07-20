170e80ba598b4edb7c3f7fa7390b4443dd2f872d
Lucas de Angelo <lucasdeangelo343@gmail.com>
2023-07-20 11:21:38 -0300

    first commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,12 @@ module.exports = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      colors: {
+        'roxo': '#3D0066',
+        'bege': '#FFFCF2',
+        'amarelo': '#FFD500',
+        'preto': '#1E1E1E',
+      },
     },
   },
   plugins: [],
